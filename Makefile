1fac77340f347bba78c1b781616d94ac2303850d
Lorenzo Bolla <lorenzo.bolla@yougov.com>
2018-09-08 17:12:48 +0100

    Works with latest Rep

---
+++ Makefile
@@ -0,0 +1,5 @@
+# Based on https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_first_WebExtension
+
+# https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Package_your_extension_
+package:
+	zip -r -FS /tmp/fuckrep.zip *
--- fuckrep.js
+++ fuckrep.js
@@ -1,8 +1,27 @@
-var period = 3000;
-function fuckrep() {
-    setTimeout(fuckrep, period);
-    var a = document.getElementsByClassName('detail-article_body')[0];
-    a.children[0].style.display = 'none';
-    a.children[1].style.display = 'block';
+var period = 1000;
+
+function httpGet(theUrl) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+  xmlHttp.send( null );
+  return xmlHttp.responseText;
 }
+
+function fill(content) {
+  var a = document.getElementsByClassName('detail-article_body');
+  if (a.length > 0) {
+    a[0].innerHTML = content;
+  } else {
+    setTimeout(function() { fill(content) }, period);
+  }
+}
+
+function fuckrep() {
+  var pathname = window.location.pathname;
+  var url = 'https://rep.repubblica.it/ws/detail/' + pathname.substring(5) + 'content.json';
+  var data = JSON.parse(httpGet(url));
+  var content = data[0].content + '<h2>FuckRep!</h2>';
+  fill(content);
+}
+
 fuckrep();
