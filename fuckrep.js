bf917b8ca53d867c26a35c0162a9886153c8d411
uf0 <scnepa222@gmail.com>
2019-07-12 11:19:29 +0200

    fixed selector for shadow-dom

--- fuckrep.js
+++ fuckrep.js
@@ -1,28 +1,51 @@
-var period = 1000, nretries = 6;
+var period = 1000,
+  nretries = 6;
 
 function httpGet(theUrl) {
   var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-  xmlHttp.send( null );
+  xmlHttp.open("GET", theUrl, false); // false for synchronous request
+  xmlHttp.send(null);
   return xmlHttp.responseText;
 }
 
 function fill(content, n) {
-  var a = document.getElementsByClassName('detail-article_body');
-  if (a.length > 0) {
-    a[0].innerHTML = content;
-  } else if (n < nretries) {
-    setTimeout(function() { fill(content, n + 1) }, period);
-  } else {
-    document.body.innerHTML = content;
+  try {
+    var a = document
+      .querySelector("news-app")
+      .shadowRoot.querySelector("news-article")
+      .shadowRoot.querySelector(".amp-doc-host")
+      .shadowRoot.querySelector(".detail-article_body > .paywall");
+
+    var b = document
+      .querySelector("news-app")
+      .shadowRoot.querySelector("news-article")
+      .shadowRoot.querySelector(".amp-doc-host")
+      .shadowRoot.querySelector(
+        '.detail-article_body > div[subscriptions-section="content-not-granted"]'
+      );
+
+    a.style.setProperty("display", "block", "important");
+    b.style.setProperty("display", "none");
+    a.innerHTML = content;
+  } catch (e) {
+    if (n < nretries) {
+      setTimeout(function() {
+        fill(content, n + 1);
+      }, period);
+    } else {
+      document.body.innerHTML = content;
+    }
   }
 }
 
 function fuckrep() {
   var pathname = window.location.pathname;
-  var url = 'https://rep.repubblica.it/ws/detail/' + pathname.substring(5) + 'content.json';
+  var url =
+    "https://rep.repubblica.it/ws/detail/" +
+    pathname.substring(5) +
+    "content.json";
   var data = JSON.parse(httpGet(url));
-  var content = data[0].content + '<h2>FuckRep!</h2>';
+  var content = data[0].content + "<h2>FuckRep!</h2>";
   fill(content, 0);
 }
 
